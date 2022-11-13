b287c02e45ec7b878e1852d09254e0db2fc27793
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-11-13 16:47:44 +0700

    add env

--- knexCon.js
+++ knexCon.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "testdb",
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
   },
 });
 
--- routes/barang.js
+++ routes/barang.js
@@ -14,35 +14,42 @@ const barang = {
     const nama_barang = req.query.nama_barang + "";
     const kode_barang = req.query.kode_barang;
     const id = req.query.id;
+    try {
+      if (nama_barang) {
+        //console.log(nama_barang);
+        const detailBarang = await knex("satuan")
+          .join("barang", "barang.kode_barang", "=", "satuan.kode_barang")
+          .select(
+            "barang.nama_barang",
+            "satuan.kode_satuan",
+            "satuan.besar_konversi",
+            "satuan.harga"
+          )
+          .where(
+            "barang.nama_barang",
+            "like",
+            `%${nama_barang.toUpperCase()}%`
+          );
+        //res.send("ada");
+        response.success(detailBarang, res);
+      }
+      if (kode_barang) {
+        const detailBarang = await knex("satuan")
+          .join("barang", "barang.kode_barang", "=", "satuan.kode_barang")
+          .select("satuan.kode_satuan", "satuan.besar_konversi", "satuan.harga")
+          .where("barang.kode_barang", kode_barang);
+        response.success(detailBarang, res);
+      }
 
-    if (nama_barang) {
-      //console.log(nama_barang);
-      const detailBarang = await knex("satuan")
-        .join("barang", "barang.kode_barang", "=", "satuan.kode_barang")
-        .select(
-          "barang.nama_barang",
-          "satuan.kode_satuan",
-          "satuan.besar_konversi",
-          "satuan.harga"
-        )
-        .where("barang.nama_barang", "like", `%${nama_barang.toUpperCase()}%`);
-      //res.send("ada");
-      response.success(detailBarang, res);
-    }
-    if (kode_barang) {
-      const detailBarang = await knex("satuan")
-        .join("barang", "barang.kode_barang", "=", "satuan.kode_barang")
-        .select("satuan.kode_satuan", "satuan.besar_konversi", "satuan.harga")
-        .where("barang.kode_barang", kode_barang);
-      response.success(detailBarang, res);
-    }
-
-    if (id) {
-      const detailBarang = await knex("satuan")
-        .join("barang", "barang.kode_barang", "=", "satuan.kode_barang")
-        .select("satuan.kode_satuan", "satuan.besar_konversi", "satuan.harga")
-        .where("barang.id", id);
-      response.success(detailBarang, res);
+      if (id) {
+        const detailBarang = await knex("satuan")
+          .join("barang", "barang.kode_barang", "=", "satuan.kode_barang")
+          .select("satuan.kode_satuan", "satuan.besar_konversi", "satuan.harga")
+          .where("barang.id", id);
+        response.success(detailBarang, res);
+      }
+    } catch (error) {
+      response.error(error, res);
     }
   },
 };
